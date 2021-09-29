66b021a3a46dc3977b04c26b504b52f2101d523a
Bedomax <bedomax@gmail.com>
2021-09-28 23:42:59 -0500

    Day 1: Report Repair - Part 1, find sum of 2020 and multiply values

--- lib/01/report_repair.rb
+++ lib/01/report_repair.rb
@@ -1,9 +1,15 @@
+require_relative '../../helpers/file_helper'
+
 class ReportRepair
-  attr_reader :name
+  PATH_FILE = "./assets/01/input.txt"
+  attr_reader :expenses
   def initialize
-    @name = "bedo"
+    @expenses = FileHelper.read_file_to_array(path:PATH_FILE, integer: true)
   end
-  def call
-    @name
+  def part_1
+    pairs = @expenses.combination(2).find_all do |a, b|
+      a + b == 2020
+    end
+    pairs.flatten.inject(:*)
   end
 end
