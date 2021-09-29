6aed518bfdd47f42a60ffa53887e75c14b15b3cb
Bedomax <bedomax@gmail.com>
2021-09-29 01:48:33 -0500

    Day 2: Password Philosophy - Part 2 - Validate password - rule structure and positions

--- lib/02/password_philosophy.rb
+++ lib/02/password_philosophy.rb
@@ -8,22 +8,33 @@ class PasswordPhilosophy
     @passwords = FileHelper.read_file_to_array(path:PATH_FILE)
   end
 
-  def part_1
-    @passwords.filter(&method(:validate_password)).size
+  def validate_structure
+    @passwords.filter(&method(:validate_structure_rule)).size
+  end
+
+  def validate_position
+    @passwords.filter(&method(:validate_position_rule)).size
   end
 
   private
 
-  def validate_password(row)
+  def validate_structure_rule(row)
     password, rule = parse_row(row)
     password_size = password.chars.filter { |char| char == rule[:letter]}.size
-    rule[:min] <= password_size && password_size <= rule[:max]
+    min, max = rule[:num_1], rule[:num_2]
+    min <= password_size && password_size <= max
+  end
+
+  def validate_position_rule(row)
+    password, rule = parse_row(row)
+    position_1 , position_2 = rule[:num_1] - 1 , rule[:num_2] - 1
+    (password[position_1] == rule[:letter]) ^ (password[position_2] == rule[:letter])
   end
 
   def parse_row(row)
     unparsed, password = row.split(': ')
     min_max, letter = unparsed.split(' ')
-    min, max = min_max.split('-').map(&:to_i)
-    [password, {letter: letter, min: min, max: max}]
+    min,max = min_max.split('-').map(&:to_i)
+    [password.strip, {letter: letter, num_1: min, num_2: max}]
   end
 end
--- spec/02/password_philosophy.rb
+++ spec/02/password_philosophy.rb
@@ -2,7 +2,12 @@ require_relative '../../lib/02/password_philosophy'
 
 describe PasswordPhilosophy do
   let(:password_philosophy) { PasswordPhilosophy.new }
-  it 'solve part 1' do
-    expect(password_philosophy.part_1).to eq(655)
+  describe 'Count how many passwords have' do
+    it 'structure rule - solve part 1' do
+      expect(password_philosophy.validate_structure).to eq(655)
+    end
+    it 'position rule  - solve part 2' do
+      expect(password_philosophy.validate_position).to eq(673)
+    end
   end
 end
